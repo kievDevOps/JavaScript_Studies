4208fec9575ab03c90c2ec1d723832cf89f0e7e1
kiev <kievdevops@gmail.com>
2024-12-06 17:46:15 -0300

    typeConversion
    How to change the datatype of a value to another

---
+++ typeConversion.js
@@ -0,0 +1,7 @@
+//  Type Conversion = change the datatype of a value to another
+//  (strings, numbers, booleans)
+
+let age = window.prompt("How old are you?"); //  string
+age = Number(age); //  changes the value from string to number
+age += 1;
+console.log(age, typeof age);
