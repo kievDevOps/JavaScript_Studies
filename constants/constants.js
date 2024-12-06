f41bcf2b90f95f310149fc9fe982a011dd0ebbf9
kiev <kievdevops@gmail.com>
2024-12-06 17:58:40 -0300

    constants
    Variables that can't be changed

---
+++ constants/constants.js
@@ -0,0 +1,17 @@
+//  const = a variable that can't be changed
+
+const PI = 3.14159;
+let radius;
+let circumference;
+
+radius = window.prompt("Enter the radius of a circle:");
+radius = Number(radius);
+
+circumference = 2 * pi * radius;
+
+document.getElementById("mySubmit").onclick = function () {
+  radius = document.getElementById("text").value;
+  radius = Number(radius);
+  circumference = 2 * PI * radius;
+  document.getElementById("myH3").textContent = circumference + "cm";
+};
