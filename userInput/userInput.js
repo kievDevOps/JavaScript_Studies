fb40b83db21ae3e302f7a8b71a8819d55aee07ab
kiev <kievdevops@gmail.com>
2024-12-06 17:33:35 -0300

    userInput
    How to access user input through ''Window prompt'' and ''HTML textbox''

---
+++ userInput/userInput.js
@@ -0,0 +1,14 @@
+// How to accept user input
+
+// --- Window Prompt --- //
+// let username;
+// username = window.prompt("What's your username?");
+// console.log(username);
+// --------------------- //
+
+// --- HTML Textbox --- //
+document.getElementById("mySubmit").onclick = function () {
+  username = document.getElementById("myText").value;
+  console.log(username);
+  document.getElementById("myH1").textContent = "Hello ${username}";
+};
