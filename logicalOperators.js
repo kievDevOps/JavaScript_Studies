1a48a256f4ed51b193f3bb625d111520e1ec77a8
kiev <kievdevops@gmail.com>
2024-12-09 14:49:21 -0300

    logicalOperators
    Combining and manipulating boolean values

---
+++ logicalOperators.js
@@ -0,0 +1,21 @@
+//  logical operators = used to combine or manipulate boolean values (true or false)
+
+//  AND = &&
+//  OR = ||
+//  NOT = !
+
+const temp = 20;
+if (temp > 0 && temp <= 30) {
+  console.log("The temperature is good");
+} else {
+  console.log("The temperature is bad");
+}
+
+//  ------------------------------------------  //
+
+const isSunny = true;
+if (!isSunny) {
+  console.log("It's cloudy!");
+} else {
+  console.log("It's sunny!");
+}
