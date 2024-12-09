72b6a7ffa508463683a3c3f907b2e6299a324c61
kiev <kievdevops@gmail.com>
2024-12-09 15:12:15 -0300

    whileLoops
    How to repeat codes until some conditions are met

---
+++ whileLoops.js
@@ -0,0 +1,28 @@
+//  while loop = repeat some code while some condition is true
+
+let username = "";
+
+while (username === "" || username === null) {
+  username = window.prompt("Please enter your username");
+}
+
+console.log("Hello ${username}");
+
+// ---------------------------------------------------------- //
+
+let loggedIn = false;
+let username2;
+let password;
+
+while (!loggedIn) {
+  // ! = NOT
+  username2 = window.prompt("Enter your username");
+  password = window.prompt("Enter your password");
+
+  if (username === "myUsername" && password === "myPassword") {
+    loggedIn = true;
+    console.log("Welcome! you are logged in");
+  } else {
+    console.log("Incorrect username or password");
+  }
+}
