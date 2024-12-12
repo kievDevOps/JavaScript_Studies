1dcdebc77b9c9ec14356404d2088c84a89de7824
kiev <kievdevops@gmail.com>
2024-12-12 17:57:50 -0300

    userInput

--- userInput/userInput.js
+++ userInput/userInput.js
@@ -10,5 +10,5 @@
 document.getElementById("mySubmit").onclick = function () {
   username = document.getElementById("myText").value;
   console.log(username);
-  document.getElementById("myH1").textContent = "Hello ${username}";
+  document.getElementById("myH1").textContent = `Hello ${username}`;
 };
