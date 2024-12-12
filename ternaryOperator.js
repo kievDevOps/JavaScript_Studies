d345ad6776b2fc3a213e90e5692c0d62cb5154e9
kiev <kievdevops@gmail.com>
2024-12-12 17:49:12 -0300

    ternaryOperator

--- ternaryOperator.js
+++ ternaryOperator.js
@@ -23,5 +23,5 @@ console.log(question);
 let purchaseAmount = 125;
 let discount = purchaseAmount >= 100 ? 10 : 0;
 console.log(
-  "Your total is $${purchaseAmount - purchaseAmount * (discount / 100)}"
+  `Your total is $${purchaseAmount - purchaseAmount * (discount / 100)}`
 );
