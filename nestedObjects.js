896968d9e8ffe39e9f6664e1a81ab6431403855a
kiev <kievdevops@gmail.com>
2024-12-14 19:59:28 -0300

    nestedObjects
    Objects inside of other objects

---
+++ nestedObjects.js
@@ -0,0 +1,45 @@
+//  nested objects = Objects inside of other objects. Allows you to represent more complex data structures.
+//  Child object is enclosed by a Parent object.
+
+//  Person{Address{}, ContactInfo{}}
+//  ShoppingCart{Keyboard{}, Mouse{}, Monitor{}}
+
+class Person {
+  constructor(name, age, ...address) {
+    this.name = name;
+    this.age = age;
+    this.address = new Address(...address);
+  }
+}
+
+class Address {
+  constructor(street, city, country) {
+    this.street = street;
+    this.city = city;
+    this.country = country;
+  }
+}
+
+const person1 = new Person(
+  "Spongebob",
+  30,
+  "124 Conch St.",
+  "Bikini Bottom",
+  "Int. Waters"
+);
+const person2 = new Person(
+  "Patrick",
+  37,
+  "128 Conch St.",
+  "Bikini Bottom",
+  "Int. Waters"
+);
+const person3 = new Person(
+  "Squidward",
+  45,
+  "126 Conch St.",
+  "Bikini Bottom",
+  "Int. Waters"
+);
+
+console.log(person1.address.country);
