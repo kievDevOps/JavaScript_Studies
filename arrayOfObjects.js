c454fef9bceca4c61d7477f8a55c986a00a19bc7
kiev <kievdevops@gmail.com>
2024-12-14 20:08:37 -0300

    arrayOfObjects

---
+++ arrayOfObjects.js
@@ -0,0 +1,10 @@
+const fruits = [
+  { name: "apple", color: "red", calories: 95 },
+  { name: "orange", color: "orange", calories: 45 },
+  { name: "banana", color: "yellow", calories: 105 },
+  { name: "coconut", color: "white", calories: 159 },
+  { name: "pineapple", color: "yellow", calories: 37 },
+];
+
+fruits.push({ name: "grapes", color: "purple", calories: 62 });
+fruits.pop();
